4295d1320de58a1fd3666c13565ecfcd884751e9
Mathieu <mathieu.linder.dev@gmail.com>
2021-11-01 17:00:41 +0100

    CSS and form validation

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,12 +4,11 @@ const Weather = (props) => {
 
    const roundTemp = Math.round(data.main.temp);
    const imgCondition = `https://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`;
-
    
    let today = new Date();
    let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
    let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-   let dateTime =  Date.parse(date+' '+time);
+   let dateTime =  Date.parse(`${date} ${time}`);
    let shift = dateTime + data.timezone*1000 - 7200*1000;
    let newDate = new Date(shift).toLocaleDateString("fr-FR", {
       hour: "numeric",
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,28 +2,37 @@ import React, { useState } from 'react';
 import Weather from '../components/Weather';
 
 const Home = () => {
-   const [city, setCity] = useState('');
-   const [data, setData] = useState({});
+   let [city, setCity] = useState('');
+   const [data, setData] = useState();
+
  
    const displayWeather = (e) => {
       e.preventDefault();
+      if (city === '') {
+         document.querySelector('.empty').classList.add('active');
+      } else {         
+         document.querySelector('.weather').classList.add('active');
+         document.querySelector('.empty').classList.remove('active');
 
-      const apiKey = '38927430cea36d378172a1106e55c374';
-      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}&units=metric`)
+         const apiKey = '38927430cea36d378172a1106e55c374';
+         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}&units=metric&lang=fr`)
          .then((res) => res.json())
          .then((data) => {
             setData(data);
-            });
-   };
+         });
+         setCity('');
+      };
+   }
 
    return (
       <div className="container">
-         <form>
-            <input type="text" placeholder="City" value={city} onChange={(e) => setCity(e.target.value)}/>
+         <p className="empty">Veuillez saisir une ville</p>
+         <form noValidate>
+            <input type="text" placeholder="City" required value={city} onChange={(e) => setCity(e.target.value)}/>
             <input type="submit" value="Valider" onClick={displayWeather}/>
          </form>
          <div className="weather">
-            { data.name && <Weather data={data} />}
+            { data ? <Weather data={data} /> : ""}
          </div>
       </div>
    );
